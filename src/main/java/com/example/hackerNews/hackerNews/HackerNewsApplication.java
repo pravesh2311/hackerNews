57bb64122031ca75a015a61a1a65971bf37e2921
pravesh2311 <pravees0987@gmail.com>
2023-04-02 11:34:04 +0530

    hackerNews: Setup

---
+++ src/main/java/com/example/hackerNews/hackerNews/HackerNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackerNews.hackerNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerNewsApplication.class, args);
+	}
+
+}
